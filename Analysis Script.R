976261ed8b69ce686ee4b4311fb99c4c8cae3a89
Kwasi48 <abuwestwood@gmail.com>
2024-12-30 06:10:53 +0000

    updating the script

--- Analysis Script.R
+++ Analysis Script.R
@@ -43,7 +43,8 @@ glimpse(Mobile_phone1)
 #group mobile phones by Region 
 Region_data <- Mobile_phone1 |> 
   select(region, location) |> 
-  group_by(region) |>
+  group_by(region) 
+
 Region_data
 
 Region_data <- Mobile_phone1 |> 
