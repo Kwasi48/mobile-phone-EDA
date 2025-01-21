14a16c4897588899d94799f03466555c64308b8d
Kwasi48 <abuwestwood@gmail.com>
2025-01-21 14:36:39 +0000

    never stopping

--- bootstraspping.R
+++ bootstraspping.R
@@ -1,4 +1,9 @@
 #Bootstrapping and others 
+library(tidyverse)
+library(mdsr)
+library(nycflights13)
+
+
 
 three_flights <- SF |> 
   slice_sample(n = 3, replace = FALSE) |>
@@ -7,10 +12,66 @@ three_flights
 
 three_flights |> slice_sample(n = 3, replace = TRUE)
 
+
 n <- 200
+
 orig_sample <- SF |> 
   slice_sample(n = n, replace = FALSE)
 
 orig_sample |> 
   slice_sample(n = n, replace = TRUE) |> 
   summarise(mean_arr_delay = mean(arr_delay))
+
+
+sf_2009_bs <- 1:num_trials |> 
+  map_dfr(
+    ~orig_sample |> 
+      slice_sample( n = n, replace = TRUE) |> 
+      summarise (mean_arr_delay = mean(arr_delay))
+  ) |> 
+  mutate(n = n)
+
+sf_2009_bs |> 
+  skim(mean_arr_delay)
+
+sf_200_pop <- 1:num_trials |> 
+  map_dfr(
+    ~SF |> 
+      slice_sample(n = n, replace = TRUE) |>
+      summarise(mean_arr_delay = mean(arr_delay))
+  ) |> 
+  mutate(n = n )
+ 
+
+sf_200_pop |> 
+  skim(mean_arr_delay)
+
+
+orig_sample |> 
+  summarise(q98 = quantile(arr_delay, p = 0.98))
+
+n <- nrow(orig_sample)
+sf_2009_bs <- 1:num_trials |> 
+  map_dfr(
+    ~orig_sample |> 
+      slice_sample(n = n, replace =  TRUE) |> 
+      summarise(q98 = quantile(arr_delay, p = 0.98))
+  )
+
+
+sf_2009_bs |>
+  skim(q98)
+ 
+set.seed(1001)
+n_large <- 10000
+sf_10000_bs <- SF |> 
+  slice_sample(n = n_large, replace = FALSE)
+
+sf_2009_bs <- 1:num_trials |>
+  map_dfr(~sf_10000_bs |>
+            slice_sample(n = n_large, replace = TRUE) |>
+            summarize(q98 = quantile(arr_delay, p = 0.98))
+  )
+
+sf_2009_bs |>
+  skim(q98)
